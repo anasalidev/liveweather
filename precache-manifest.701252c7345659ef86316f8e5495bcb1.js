b8d3c601f897dce571460d7e04f7b61d4cb88fc7
Anas <anasalibinanwar@gmail.com>
2023-09-06 23:03:57 +0500

    Updates

---
+++ precache-manifest.701252c7345659ef86316f8e5495bcb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39f27605fbd6481bfb01",
+    "url": "/liveweather/static/css/main.ebf33237.chunk.css"
+  },
+  {
+    "revision": "39f27605fbd6481bfb01",
+    "url": "/liveweather/static/js/main.39f27605.chunk.js"
+  },
+  {
+    "revision": "a5480ee66881d0f8ab76",
+    "url": "/liveweather/static/js/1.a5480ee6.chunk.js"
+  },
+  {
+    "revision": "840b06396323b7141972",
+    "url": "/liveweather/static/js/runtime~main.840b0639.js"
+  },
+  {
+    "revision": "3a646b48404a7fb20ef46142107df049",
+    "url": "/liveweather/index.html"
+  }
+];
